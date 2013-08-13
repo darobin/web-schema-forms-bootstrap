ea6701eefec6b28ddb0fd6afa1a578afc29100bd
Robin Berjon <robin@berjon.com>
2013-08-13 19:10:22 +0200

    switch to new Web Schema Forms approach

--- README.md
+++ README.md
@@ -3,11 +3,23 @@
 
 This plugin enhanced Web Schema Forms with the classes required for Bootstrap.
 
-## Hints
+## Usage
 
-The following hints are supported:
+    var wsf = require("web-schema-forms")
+    ,   bootstrap = require("web-schema-forms-bootstrap")
+    ;
 
-* ```bootstrap_type```. Can be set to ```basic``` (the default), ```inline```, or ```horizontal```.
-  This basically configures the type of form you want, causing the full template to behave
-  accordingly.
+    var form = wsf.form({
+                            extensions: [bootstrap.horizontal]
+                        ,   schema: { ... } // web schema
+                        ,   hints:  { ... } // rendering hints
+                        }
+                    ,   function (err, form) {
+                            if (err) return console.log("ERROR", err);
+                            // ...
+                        }
+    );
 
+There are three types of Bootstrap 3 forms supported by this module: ```basic```, ```inline```, or
+```horizontal```. Each is an extension of its own, available as an attribute on the module. Pass
+one to the ```extensions``` field for Web Schema Forms' ```form()``` method and enjoy the result.
--- index.js
+++ index.js
@@ -1,7 +1,83 @@
 
-var pth = require("path")
-;
+function btn ($form) {
+    $form.find("button[type=submit]").addClass("btn btn-default");
+}
 
-exports.init = function (wsf) {
-    wsf.register("bootstrap", pth.resolve(pth.join(__dirname, "bootstrap")));
+function checkbox ($form, $) {
+    $form.find("div[data-wsf-type=boolean]")
+         .addClass("checkbox")
+         .each(function () {
+             var $chk = $(this)
+             ,   $label = $chk.find("label")
+             ,   $ctrl = $chk.find("input, textarea, select")
+             ;
+             $label.text(" " + $label.text())
+                   .prepend($ctrl)
+                   .removeAttr("for")
+                   .attr("data-wsf-type", "boolean")
+                   ;
+         })
+         ;
+}
+
+exports.basic = function (context, cb) {
+    var $form = context.$form;
+    $form.find("div[data-wsf-type]")
+         .not("[data-wsf-type=boolean]")
+         .addClass("form-group")
+         .find("input, textarea, select")
+            .addClass("form-control")
+         ;
+    checkbox($form, context.$);
+    btn($form);
+    cb(null, context);
+};
+
+exports.inline = function (context, cb) {
+    var $form = context.$form
+    ,   $ = context.$
+    ;
+    $form.addClass("form-inline");
+    checkbox($form, $);
+    $form.find("div[data-wsf-type]")
+         .not("[data-wsf-type=actions]")
+         .not("[data-wsf-type=boolean]")
+         .each(function () {
+             var $div = $(this)
+             ,   $ctrl = $div.find("input, textarea, select").first()
+             ,   text = $div.find("label").text()
+             ;
+             if (!$ctrl.attr("placeholder") && !$ctrl.is("select")) $ctrl.attr("placeholder", text);
+             $ctrl.attr("data-wsf-type", $div.attr("data-wsf-type"));
+             $div.replaceWith($ctrl);
+         })
+         ;
+    $form.find("div[data-wsf-type=actions]")
+         .each(function () {
+             var $div = $(this);
+             $div.replaceWith($div.find("button"));
+         })
+         ;
+    btn($form);
+    cb(null, context);
+};
+
+exports.horizontal = function (context, cb) {
+    var $form = context.$form
+    ,   $ = context.$
+    ;
+    $form.addClass("form-horizontal");
+    checkbox($form, $);
+    $form.find("div[data-wsf-type]")
+         .not("[data-wsf-type=actions]")
+         .not("[data-wsf-type=boolean]")
+         .each(function () {
+             var $div = $(this);
+             $div.find("label").addClass("col-lg-2 control-label");
+             $div.find("input, select, textarea").wrapAll("<div class='col-lg-10'></div>");
+         })
+         ;
+    $form.find("div[data-wsf-type=actions] button").wrapAll("<div class='col-lg-offset-2 col-lg-10'></div>");
+    btn($form);
+    cb(null, context);
 };
